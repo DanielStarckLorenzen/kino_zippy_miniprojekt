2c4c5e1ec02fbc36d50891c905e6ec55a46a54e3
daniellorenzen <dani141d@stud.kea.dk>
2023-03-13 10:09:20 +0100

    first commit

---
+++ src/main/java/com/example/kino_zippy_miniprojekt/KinoZippyMiniprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.kino_zippy_miniprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoZippyMiniprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoZippyMiniprojektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kino_zippy_miniprojekt/KinoZippyMiniprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kino_zippy_miniprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoZippyMiniprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
